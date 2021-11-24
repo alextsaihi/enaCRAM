0cf1b0544db317eb5cab90681b97c7e692b6a390
alextsaihi <alextsaihi@gmail.com>
2021-11-23 21:48:05 -0500

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "alextsaihi",
+    version = "1.0",
+    author = "Alex Tsai",
+    author_email = "alextsaihi@gmail.com",
+    description = "ENA CRAM Client",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/alextsaihi/enaCRAM",
+    packages = setuptools.find_packages(),
+    python_requires  = '>=3.6',
+)
+
