93e6572f89ef3d9f0ea73cea03b33cd8ff6da5cc
alextsaihi <alextsaihi@gmail.com>
2021-11-23 22:07:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/alextsaihi/enaCRAM",
-    packages = setuptools.find_packages(),
+    packages = ['client.ena_cram.py'],
     python_requires  = '>=3.6',
 )
 
